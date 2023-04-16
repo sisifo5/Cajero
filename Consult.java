5f2a20e4a3be6f3353c0da18fca342e4a427bae4
ArDroWeb <97034442+sisifo5@users.noreply.github.com>
2023-04-15 21:36:41 -0600

    Create Consult.java

---
+++ Consult.java
@@ -0,0 +1,17 @@
+package puno;
+/*Esta clase extiende las funciones de la clase padre los metodos y atributos*/
+/**
+ *
+ * @author paranoidandroid
+ */
+
+public class Consult extends Eresismo {
+
+/*Metodo poliforma*/
+@Override
+public void Tra(){
+    System.out.println("-----------------------");
+    System.out.println("SALDO ACTUAL:" + getv5());
+    System.out.println("-----------------------");
+}
+}
