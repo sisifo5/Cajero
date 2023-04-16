e4b9ba5b303817d8b0c16b8662b43d80417d760d
ArDroWeb <97034442+sisifo5@users.noreply.github.com>
2023-04-15 21:40:58 -0600

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,18 @@
+package puno;
+
+/**
+ *
+ * @author paranoidandroid
+ */
+public class Principal {
+    public static void main(String [] args){
+        
+        Eresismo msj= new Consult();
+        msj.setv5(1500);
+        msj.Op();
+    
+    
+    
+    }
+    
+}
